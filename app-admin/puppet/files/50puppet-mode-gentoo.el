f6eb97835837c1968a7361cf60e06f2d1bd8e5ff
TANABE Ken-ichi <nabeken@tknetworks.org>
2008-11-24 12:50:30 +0900

    app-admin/puppet: removed due to including main tree

--- app-admin/puppet/files/50puppet-mode-gentoo.el
+++
@@ -1,8 +0,0 @@
-;;
-;; Setup puppet-mode for autoloading
-
-;;; puppet-mode site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'puppet-mode "puppet-mode" "Major mode for editing puppet manifests")
-(add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
